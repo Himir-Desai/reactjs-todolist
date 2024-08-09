d0a2a2004e0ffafbd94c41a3ca7a1e93446b0c79
Himir Desai <himirdesai72@gmail.com>
2024-08-10 02:06:00 +0530

    Better UI design

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 export default function TodoInput(props) {
     const {addTodo, todoValue, setTodoValue} = props;
     
     return (
         <header>
-            <input value={todoValue} onChange={(e)=>{setTodoValue(e.target.value)}} placeholder='Enter Todo ...'/>
+            <input value={todoValue} onChange={(e)=>{setTodoValue(e.target.value)}} placeholder='Enter Todo...'/>
             <button onClick={()=>{
                 if (todoValue != "") {
                     addTodo(todoValue)
